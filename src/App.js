191dbfb47027fd7d83d80f7cfb2017020aa088d8
chris <christian.Herrera01@student.csulb.edu>
2019-11-29 20:59:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import NavBar from "./Components/NavBar";
+// import Product from "./Components/Product";
+import ProductList from "./Components/ProductList";
+import Details from "./Components/Details";
+import Cart from "./Components/Cart";
+import Default from "./Components/Default";
+import Modal from "./Components/Modal"
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+
+        <Switch>
+          <Route exact={true} path="/" component={ProductList} />
+
+          <Route exact={true} path="/cart" component={Cart} />
+
+          <Route exact={true} path="/details" component={Details} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../context.js";
+export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="Products" />
+
+            <div className="row">
+              <ProductConsumer>
+                {
+                  (value) => {
+                    console.log(value);
+                    return value.products.map(product => {
+                      return <Product key={product.id} product={product}>
+
+                      </Product>
+                    });
+                  }
+
+                }
+              </ProductConsumer>
+            </div>
+
+          </div>
+        </div>
+      </React.Fragment>
+      // <Product></Product>
+    );
+  }
+}
